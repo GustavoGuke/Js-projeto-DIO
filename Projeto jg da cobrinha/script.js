5ac8a21280f380bf0aa36eb4848ec8b5b3c23ca9
Gustavo <gustavo.athenas13@gmail.com>
2020-05-23 13:24:21 -0300

    criado a comidinha

--- Projeto jg da cobrinha/script.js
+++ Projeto jg da cobrinha/script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = 'right'
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = 'lightgreen'
@@ -20,6 +24,11 @@ function criarCobrinha(){
     }
 
 }
+function drawFood(){
+    context.fillStyle = 'red'
+    context.fillRect(food.x, food.y, box, box)
+}
+
 
 document.addEventListener('keydown', update)
 
@@ -39,6 +48,7 @@ function iniciarJogo(){
 
     criarBG()
     criarCobrinha()
+    drawFood()
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
